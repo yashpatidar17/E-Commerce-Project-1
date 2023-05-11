ae4ccc8fcea3cae31245ac0638c9c9bef80aa8fb
Yash Patidar <yashpatidar964@gmail.com>
2023-05-12 01:00:56 +0530

    created project and folder structure

---
+++ PlantKart/src/App.jsx
@@ -0,0 +1,16 @@
+import { Link } from 'react-router-dom'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div>
+      <nav>
+        <Link></Link>
+      </nav>
+    </div>
+  )
+}
+
+export default App
